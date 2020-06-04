df2ef09950eb9f4af93c8b5229a092e1e33f8a8c
Toshiki Teramura <toshiki.teramura@gmail.com>
2020-06-04 18:40:42 +0900

    Setup from PyO3/rust-numpy example

---
+++ src/lib.rs
@@ -0,0 +1,38 @@
+use ndarray::{ArrayD, ArrayViewD, ArrayViewMutD};
+use numpy::{IntoPyArray, PyArrayDyn};
+use pyo3::prelude::{pymodule, Py, PyModule, PyResult, Python};
+
+#[pymodule]
+fn rust_ext(_py: Python, m: &PyModule) -> PyResult<()> {
+    // immutable example
+    fn axpy(a: f64, x: ArrayViewD<f64>, y: ArrayViewD<f64>) -> ArrayD<f64> {
+        a * &x + &y
+    }
+
+    // mutable example (no return)
+    fn mult(a: f64, mut x: ArrayViewMutD<f64>) {
+        x *= a;
+    }
+
+    // wrapper of `axpy`
+    #[pyfn(m, "axpy")]
+    fn axpy_py(
+        py: Python,
+        a: f64,
+        x: &PyArrayDyn<f64>,
+        y: &PyArrayDyn<f64>,
+    ) -> Py<PyArrayDyn<f64>> {
+        let x = x.as_array();
+        let y = y.as_array();
+        axpy(a, x, y).into_pyarray(py).to_owned()
+    }
+
+    // wrapper of `mult`
+    #[pyfn(m, "mult")]
+    fn mult_py(_py: Python, a: f64, x: &PyArrayDyn<f64>) -> PyResult<()> {
+        let x = x.as_array_mut();
+        mult(a, x);
+        Ok(())
+    }
+    Ok(())
+}
